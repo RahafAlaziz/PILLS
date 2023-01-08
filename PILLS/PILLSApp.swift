e0ad66eb6202fe6f8528b8e6d0162dfb1419136f
rahaf <rahf.saed28@gmail.com>
2023-01-08 18:16:47 +0300

    Initial Commit

---
+++ PILLS/PILLSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PILLSApp.swift
+//  PILLS
+//
+//  Created by rahaf on 15/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct PILLSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
